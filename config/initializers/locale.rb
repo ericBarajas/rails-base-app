e91205f772f20c47572afb260edfeb5d70d4e255
Max Ivak <maxivak@gmail.com>
2015-05-13 23:03:28 +0300

    config

---
+++ config/initializers/locale.rb
@@ -0,0 +1,11 @@
+# in config/initializers/locale.rb
+
+#Rails.application.config.i18n.enforce_available_locales = false
+#I18n.enforce_available_locales = false
+
+Rails.application.config.time_zone = 'Europe/Kiev'
+
+#config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+#I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
+#I18n.default_locale = :en
