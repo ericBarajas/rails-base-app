bfaa8f6c2d6c43f751c95f000c0542e6d96e34bd
mmx <maxivak@gmail.com>
2017-09-25 15:16:55 +0300

    autoload, eager load

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module Myapp
 
 
     #
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    #config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
 
     #
     config.SITE_NAME = 'myapp'
